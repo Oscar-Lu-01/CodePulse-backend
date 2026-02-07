2ea1e31f44bd8e3f0d256960e87c336ec4814e0e
OscarLu <241607010052@hhu.edu.cn>
2026-02-07 13:27:15 +0800

    项目初始化

---
+++ src/main/java/cn/edu/hhu/codepulse/CodePulseApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.hhu.codepulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodePulseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodePulseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/edu/hhu/codepulse/CodePulseApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.hhu.codepulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodePulseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
